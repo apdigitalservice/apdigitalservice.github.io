76619d0c621b8fb28218105d6d1037b2a84e5685
apdigitalservice <info.me.np.online@gmail.com>
2026-02-16 17:57:07 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+function goToProduct(productName) {
+  localStorage.setItem("selectedProduct", productName);
+  window.location.href = "product.html";
+}
+
+
+
+
 console.log("AP Digital Service Website Loaded");
 
 // Example future functions
